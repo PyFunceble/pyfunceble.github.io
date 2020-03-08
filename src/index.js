a1e8a0bdcd1d6529cae8203355440c7ce7e8c4fa
funilrys <contact@funilrys.com>
2020-03-08 09:51:00 +0100

    Update router.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import Home from "./Home";
 import Contributors from "./Contributors";
